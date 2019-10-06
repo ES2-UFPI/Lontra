dc937ae7e0a1ea2a4d240f8494e4add22b4603b3
hector2506 <hectorbatista500@hotmail.com>
2019-10-05 23:21:12 -0300

    Protótipo inicial componente de consulta de receita

--- Lontra/App.js
+++ Lontra/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
@@ -7,6 +7,12 @@ export default function App() {
       <View style={styles.container_central}>
         <Text style={styles.texto}>Lontra</Text>
       </View>
+      <Text style={styles.pesquisa}>Pesquisa de Receitas</Text>
+      <TextInput style = {styles.input} placeholder="Digite o ingrediente" 
+    underlineColorAndroid={'transparent'} />
+      <TouchableOpacity style={styles.button}>
+      <Text style={styles.btntext}>Pesquisar</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -30,6 +36,31 @@ const styles = StyleSheet.create({
     color: 'white',
     textAlign: 'center',
     fontSize: 30,
+    fontWeight: 'bold',
+  },
+  pesquisa:{
+    fontSize: 20,
+    fontWeight: 'bold',
+    paddingBottom: 10,
+    marginTop: 25,
+  },
+  input: {
+    marginBottom: 25,
+    color: '#fff',
+    borderBottomColor: '#199187',
+    borderBottomWidth: 1,
+    fontSize: 15,
+  },
+  button:{
+    backgroundColor: '#7FFF00',
+    alignItems: "center",
+    justifyContent:  'center',
+    borderRadius: 10
+  },
+  btntext:{
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 25,
     fontWeight: 'bold',
   }
 });
