546f31d5918b1320d924a61aa5e81b4a36c7414c
Gabriel Costa <gabriel.costa.campos.13@gmail.com>
2019-10-05 21:57:56 -0300

    Adiciona tela de exibição de receita #11

--- Lontra/App.js
+++ Lontra/App.js
@@ -1,35 +1,54 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Receita from './src/Receita';
+
+const stackNavigation = createStackNavigator(
+	{
+		Receita: {
+			screen: Receita,
+			title: 'Receita'
+		},
+	},
+	{
+		defaultNavigationOptions: {
+			headerStyle: {
+				backgroundColor: 'orange',
+			},
+			headerTintColor: '#fff',
+		}
+	}
+);
+
+const Navegacao = createAppContainer(stackNavigation);
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.container_central}>
-        <Text style={styles.texto}>Lontra</Text>
-      </View>
-    </View>
-  );
+	return (
+		<Navegacao/>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  container_central:{
-    backgroundColor: 'blue',
-    width: 120,
-    height: 70,
-    alignItems: "center",
-    justifyContent:  'center',
-    borderRadius: 10
-  },
-  texto:{
-    color: 'white',
-    textAlign: 'center',
-    fontSize: 30,
-    fontWeight: 'bold',
-  }
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	container_central:{
+		backgroundColor: 'blue',
+		width: 120,
+		height: 70,
+		alignItems: "center",
+		justifyContent:  'center',
+		borderRadius: 10
+	},
+	texto:{
+		color: 'white',
+		textAlign: 'center',
+		fontSize: 30,
+		fontWeight: 'bold',
+	}
 });
