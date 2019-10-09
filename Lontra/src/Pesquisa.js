ffb3be4aecccfddb85f16a7bd9c1784772d2907e
Ian Luccas <ianlucas2503@gmail.com>
2019-10-09 14:15:32 -0300

    text input esta funcionando#4

--- Lontra/src/Pesquisa.js
+++ Lontra/src/Pesquisa.js
@@ -1,72 +1,93 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, ScrollView, Button, Icon, TextInput} from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity, ScrollView, Button, Icon, TextInput } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
 import { Ionicons } from '@expo/vector-icons';
 
 export default class telaInicial extends Component {
-    static navigationOptions = {
-        title: 'Pesquisa'
+  static navigationOptions = {
+    title: 'Pesquisa'
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      ingredientes: ['', '', '', ''],
     };
 
-    constructor(props){
-        super(props);
+    this.alerta = this.alerta.bind(this)
+    this.pegaDado = this.pegaDado.bind(this)
+
+  }
   
-        this.state = {
-            ingredientes: ['', '', '', ''],
-        };
-       
-      }
-  render(){
+  pegaDado(valor){
+    
+    let lista = this.state.ingredientes;
+    lista[item.index] = TextInputValue
+    this.setState(lista);
+  }
+
+  alerta(){
+
+    let aux = this.state.ingredientes;
+
+    alert(aux)
+  }
+  render() {
     return (
-      <View style={{marginBottom: 53, paddingLeft: 10, paddingRight: 10}}>
-      <ScrollView>
-        <View style={styles.tela}>
-          <View style={styles.areaTexto}>
-            <Text style={styles.texto}>Digite os Ingredientes:</Text>
-          </View>
-          <FlatList data={ this.state.ingredientes } renderItem={({ item }) => (
-              <View style={styles.areaInput}>
-              <TextInput placeholder= 'Ingrediente' underlineColorAndroid='blue' style={styles.input}
-                onChangeText={TextInputValue =>{
-                  let lista = this.state.ingredientes;
-                  lista[item.index] = TextInputValue
-                  this.setState({ ingredientes : lista });
-                }}/>
-                {/* Add o btn de menos aqui */}
+      <View style={{ marginBottom: 53, paddingLeft: 10, paddingRight: 10 }}>
+        <ScrollView>
+          <View style={styles.tela}>
+            <View style={styles.areaTexto}>
+              <Text style={styles.texto}>Digite os Ingredientes:</Text>
             </View>
-          )}/>
-        </View>
-          
-      </ScrollView>
+            <FlatList 
+              data={this.state.ingredientes} 
+              renderItem={({ item, index }) => (
+                <View style={styles.areaInput} key={index.toString()}>
+                  <TextInput placeholder='Ingrediente' underlineColorAndroid='blue' value={item} style={styles.input} key={index.toString()}
+                    onChangeText={TextInputValue => {
+                      let lista = this.state.ingredientes;
+                      lista[index] = TextInputValue;
+                      this.setState({ingredientes: lista});
+                    }} />
+                {/* Add o btn de menos aqui */}
+                  
+                </View>
+              )}
+            keyExtrator={(item, index )=> index.toString()}/>
+          </View>
+
+        </ScrollView>
         <View>
           <View style={{ justifyContent: 'space-between', flexDirection: 'row' }}>
             <TouchableOpacity onPress={() => {
               let ingredientes = this.state.ingredientes;
               ingredientes.push('')
-              this.setState({  ingredientes: ingredientes  })
+              this.setState({ ingredientes: ingredientes })
             }}>
-              <Ionicons name="ios-add-circle-outline" size={40}/>
+              <Ionicons name="ios-add-circle-outline" size={40} />
             </TouchableOpacity>
-              <TouchableOpacity style={styles.botao} onPress={() => {}}>
-                <View style={styles.container}>
-                  <View style={styles.container_central}>
-                    <Text style={styles.textoBotao}>Salvar Ingredientes</Text>
-                  </View>
+            <TouchableOpacity style={styles.botao} onPress= {this.alerta}>
+              <View style={styles.container}>
+                <View style={styles.container_central}>
+                  <Text style={styles.textoBotao}>Salvar Ingredientes</Text>
                 </View>
-              </TouchableOpacity>
+              </View>
+            </TouchableOpacity>
           </View>
         </View>
-        </View>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  tela:{
+  tela: {
     alignItems: 'center',
     justifyContent: 'center',
   },
-  areaInput:{
+  areaInput: {
     justifyContent: 'center',
     height: 60,
     width: 300,
@@ -74,20 +95,20 @@ const styles = StyleSheet.create({
     //padding: 20,
 
   },
-  texto:{
+  texto: {
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
     marginTop: 5,
     fontSize: 18
   },
-  areaTexto:{
-      alignItems: 'center',
-      justifyContent: 'center',
+  areaTexto: {
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  input:{
-      height: 40,
-      paddingLeft: 6
+  input: {
+    height: 40,
+    paddingLeft: 6
   },
   container: {
     backgroundColor: '#fff',
@@ -95,16 +116,16 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'flex-end',
   },
-  container_central:{
+  container_central: {
     backgroundColor: 'blue',
     width: 150,
     height: 50,
     padding: 5,
     alignItems: "center",
-    justifyContent:  'center',
+    justifyContent: 'center',
     borderRadius: 10
   },
-  textoBotao:{
+  textoBotao: {
     color: 'white',
     textAlign: 'center',
     fontSize: 15,
