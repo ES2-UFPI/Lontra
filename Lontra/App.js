ce7c91f917d537c8009660c3b537ec14274879dd
Ian Luccas <ianlucas2503@gmail.com>
2019-10-26 09:46:22 -0300

    problemas com o gitignore #master

--- Lontra/App.js
+++ Lontra/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import Recomendados from './src/Recomendados';
+//import stackNavigation from './src/PesquisarReceitas';
 import { createStackNavigator } from 'react-navigation-stack';
 
 import Receita from './src/Receita';
@@ -37,33 +40,15 @@ const stackNavigation = createStackNavigator(
 	}
 );
 
-const Navegacao = createAppContainer(stackNavigation);
 
-export default function App() {
-	return (
-		<Navegacao/>
-	);
-}
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	container_central:{
-		backgroundColor: 'blue',
-		width: 120,
-		height: 70,
-		alignItems: "center",
-		justifyContent:  'center',
-		borderRadius: 10
-	},
-	texto:{
-		color: 'white',
-		textAlign: 'center',
-		fontSize: 30,
-		fontWeight: 'bold',
-	}
+
+
+
+
+const TabNavigator = createBottomTabNavigator({
+  Recomendados: Recomendados,
+  PesquisarReceitas: stackNavigation,
 });
+
+export default createAppContainer(TabNavigator);
---
+++ Lontra/src/Recomendados.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, TouchableOpacity, Button} from 'react-native';
+
+
+export default class Recomendados extends Component {
+
+
+    constructor(props){
+      super(props);
+
+      this.state = {
+
+      };
+
+      //this.mudaPagina = this.mudaPagina.bind(this)
+     
+    }
+
+  render(){
+    return (
+      <View>
+          <Text>Oi</Text>
+
+      </View>
+    );
+  }
+}
