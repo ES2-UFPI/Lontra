d6beb4d2171dfc47f8ac52d0b6f5ff5f8d9553d7
Gabriel Costa Campos <gabriel.costa.campos.13@gmail.com>
2019-11-27 16:54:30 -0200

    Adiciona testes de criação de receita #17

--- api_lontra/lontra/tests.py
+++ api_lontra/lontra/tests.py
@@ -1,41 +1,64 @@
-from django.test import TestCase, Client
-from .models import Receita, Usuario
-from .serializers import UsuarioSerializer, ImagemSerializer, HistoricoListSerializer, CategoriaSerializer, ReceitaSerializer
-import json
+from rest_framework.test import APITestCase, APIClient
 from rest_framework import status
-from django.urls import reverse
+from django.urls import reverse, resolve 
+from .models import Receita, Usuario
 
-client = Client()
+class ReceitasTests(APITestCase):
 
-class ReceitasTest(TestCase):
-    def preparacao(self):
+    def setUp(self):
+        self.client = APIClient()
+
         Receita.objects.create(
-            nome = 'Pudim de Leite', 
-            tempo = '110', 
-            nota = '5', 
-            url = 'https://img.itdg.com.br/tdg/images/recipes/000/024/104/263980/263980_original.jpg?mode=crop&width=710&height=400', 
-            ingredientes = 'açúcar;leite condensado;leite;ovos', 
-            preparo = 'Calda; Em uma panela de fundo largo, derreta o açúcar até ficar dourado; Junte 1/2 xícara (chá) de água quente e mexa com uma colher; Deixe ferver até dissolver os torrões de açúcar e a calda engrossar; Forre com a calda uma forma com furo central (19 cm de diâmetro) e reserve; Pudim: Bata todos os ingredientes do pudim no liquidificador e despeje na forma reservada; Asse em banho-maria, em forno médio (180º C), por cerca de 1 hora e 30 minutos; Depois de frio, leve para gelar por cerca de 6 horas; Desenforme e sirva a seguir'
+            nome = "BOLO FRITO",
+            tempo = "25",
+            nota = 4,
+            url = "https://img.itdg.com.br/tdg/images/recipes/000/133/773/155774/155774_original.jpg?mode=crop&width=710&height=400",
+            ingredientes = "3/4 xícara de chá de leite;2 xícaras de chá de tapioca;1 ovo;3 colheres de sopa de margarina;1 colher de sopa de erva-doce (ou 2 colheres de sopa de queijo parmesão ralado);Sal a gosto;2 xícaras de chá de óleo aproximadamente, para a fritura",
+            preparo = "Numa bacia coloque a tapioca e o ovo.;Misture-os com as mãos.;Leve ao fogo o leite, a margarina, a erva-doce e o sal a gosto.;Quando o leite levantar fervura retire-o do fogo.;Junte o leite quente a massa.;Sove bem a massa, até que ela comece a fazer um barulhinho.;Depois de bem sovada faça tirinhas com a massa.;Enrole as tirinhas como uma trança de duas pernas.;Coloque numa panela óleo o suficiente para que os bolos fiquem boiando.;Deixe o óleo esquentar bem e coloque os bolinhos para fritar.;Tampe a panela.;Vire-os de vez em quando.;Atenção: não deixe crianças perto e tome cuidado pois eles podem estourar.;Sirva morno com café.",
         )
-        Receita.objects.create(
-            nome = 'FEIJÃO TROPEIRO', 
-            tempo = '60', 
-            nota = '5', 
-            url = 'https://img.itdg.com.br/tdg/images/recipes/000/000/961/327806/327806_original.jpg?mode=crop&width=710&height=400', 
-            ingredientes = '500g de feijão carioquinha cozido;200g de toucinho;1 concha de óleo;1 cebola média picada;4 dentes de alho;5 ovos;1 colher de sopa de sal com alho;Cheiro verde a gosto;200g de farinha de mandioca', 
-            preparo = 'Coloque o óleo em uma panela e doure a cebola, acrescente o bacon e frite bem.;Adicione o alho, sal e os ovos, misturando com cuidado para que não se despedacem muito.;Refogue o feijão, baixe o fogo, misture a farinha aos poucos e o cheiro verde.'
-        )
-        
-    def todas_as_receitas(self):
-        response = client.get(reverse('receitas'))
-        receitas = Receita.objects.all()
-        serializer = ReceitaSerializer(receitas, many=True)
-        self.assertEqual(response.data, serializer.data)
+
+    def test_todas_as_receitas(self):
+        response = self.client.get(reverse('receitas'))
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        
-    def buscar_receitas(self):
-        response = client.get(reverse('receitas/busca/?ingredientes=leite'))
-        receitas = Receita.objects.filter(ingredientes__contains='leite')
-        serializer = BuscaReceitaList(receitas, many=True)
-        self.assertEqual(response.data, serializer.data)
+
+    def test_receitas_por_ingredientes(self):
+        response = self.client.get('receitas/busca/?ingredientes=leite')
+        print(Receita.objects.count())
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_busca_vazia(self):
+        response = self.client.get('receitas/busca/?ingredientes=tatu')
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_cadastrar_receita(self):
+        receita = {
+            "nome": "BOLO FRITO",
+            "tempo": "25",
+            "nota": 4,
+            "url": "https://img.itdg.com.br/tdg/images/recipes/000/133/773/155774/155774_original.jpg?mode=crop&width=710&height=400",
+            "ingredientes": "3/4 xícara de chá de leite;2 xícaras de chá de tapioca;1 ovo;3 colheres de sopa de margarina;1 colher de sopa de erva-doce (ou 2 colheres de sopa de queijo parmesão ralado);Sal a gosto;2 xícaras de chá de óleo aproximadamente, para a fritura",
+            "preparo": "Numa bacia coloque a tapioca e o ovo.;Misture-os com as mãos.;Leve ao fogo o leite, a margarina, a erva-doce e o sal a gosto.;Quando o leite levantar fervura retire-o do fogo.;Junte o leite quente a massa.;Sove bem a massa, até que ela comece a fazer um barulhinho.;Depois de bem sovada faça tirinhas com a massa.;Enrole as tirinhas como uma trança de duas pernas.;Coloque numa panela óleo o suficiente para que os bolos fiquem boiando.;Deixe o óleo esquentar bem e coloque os bolinhos para fritar.;Tampe a panela.;Vire-os de vez em quando.;Atenção: não deixe crianças perto e tome cuidado pois eles podem estourar.;Sirva morno com café.",
+            "categorias": [4]
+        }
+
+        response = self.client.post(reverse('receitas'), receita, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_cadastrar_receita_invalida(self):
+        receita = {
+            "nome": "BOLO FRITO",
+            "tempo": "25",
+            "nota": 4,
+            "url": "https://img.itdg.com.br/tdg/images/recipes/000/133/773/155774/155774_original.jpg?mode=crop&width=710&height=400",
+            "ingredientes": "3/4 xícara de chá de leite;2 xícaras de chá de tapioca;1 ovo;3 colheres de sopa de margarina;1 colher de sopa de erva-doce (ou 2 colheres de sopa de queijo parmesão ralado);Sal a gosto;2 xícaras de chá de óleo aproximadamente, para a fritura",
+            "preparo": "Numa bacia coloque a tapioca e o ovo.;Misture-os com as mãos.;Leve ao fogo o leite, a margarina, a erva-doce e o sal a gosto.;Quando o leite levantar fervura retire-o do fogo.;Junte o leite quente a massa.;Sove bem a massa, até que ela comece a fazer um barulhinho.;Depois de bem sovada faça tirinhas com a massa.;Enrole as tirinhas como uma trança de duas pernas.;Coloque numa panela óleo o suficiente para que os bolos fiquem boiando.;Deixe o óleo esquentar bem e coloque os bolinhos para fritar.;Tampe a panela.;Vire-os de vez em quando.;Atenção: não deixe crianças perto e tome cuidado pois eles podem estourar.;Sirva morno com café.",
+            "categorias": "BOLOS E TORTAS SALGADAS"
+        }
+
+        response = self.client.post(reverse('receitas'), receita, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
